50bcb5fa49a312d6b450d28170199b16363e19d1
Waheed Ahmed <159169508+WaheedAhmed74@users.noreply.github.com>
2024-09-03 18:09:00 +0500

    Add files via upload
    PIAIC Class 06 Jupyter Notebook

---
+++ main.py
@@ -0,0 +1,12 @@
+name: str = "Soomro raJA WaheEd AhMed"
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.capitalize())
+print(name.casefold())
+print(name.center(30,"*"))
+print(len(name))
+print(dir(name))
+
+
+
